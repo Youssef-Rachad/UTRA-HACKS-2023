de24503514d7e7759202bd1503db134be403449e
Skule Webmaster <webmaster@skule.ca>
2023-03-18 15:08:21 -0400

    added ir sensor

---
+++ irsensor/irsensor.ino
@@ -0,0 +1,25 @@
+int IRSensor = 5; // connect IR sensor module to Arduino pin A5
+int LED = 1; // connect LED to Arduino pin 13
+
+void setup(){
+  Serial.begin(115200); // Init Serial at 115200 Baud Rate.
+  Serial.println("Serial Working"); // Test to check if serial is working or not
+  pinMode(IRSensor, INPUT); // IR Sensor pin INPUT
+  pinMode(LED, OUTPUT); // LED Pin Output
+}
+
+void loop(){
+  int sensorStatus = analogRead(IRSensor); // Set the GPIO as Input
+  Serial.println(sensorStatus);
+  if (sensorStatus < 512) // Check if the pin high or not
+  {
+    // if the pin is high turn off the onboard Led
+    digitalWrite(LED, LOW); // LED LOW
+    Serial.println("Motion Detected!"); // print Motion Detected! on the serial monitor window
+  }
+  else  {
+    //else turn on the onboard LED
+    digitalWrite(LED, HIGH); // LED High
+    Serial.println("Motion Ended!"); // print Motion Ended! on the serial monitor window
+  }
+}
